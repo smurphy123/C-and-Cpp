fa71b8ae6c1dd9097624ee8ceeda164bf0c89ba2
smurphy123 <smurphysmail@gmail.com>
2021-05-30 12:22:34 -0400

    Repo Update

--- Cpp/OOP/OOP/Source.cpp
+++ Cpp/OOP/OOP/Source.cpp
@@ -57,16 +57,60 @@ public:
 			std::cout << Name << ", sorry. No promotion for you." << std::endl;
 		}
 	}
+
+	virtual void Work() {
+		std::cout << Name << " is checking email, task backlog, performing tasks..." << std::endl;
+	}
+};
+
+class Developer:public Employee {
+public:
+	string FavProgrammingLanguage;
+	Developer(string name, string company, int age, string favProgrammingLanguage)
+		:Employee(name, company, age)
+	{
+		FavProgrammingLanguage = favProgrammingLanguage;
+	}
+	void FixBug() {
+		std::cout << getName << " fixed bug using " << FavProgrammingLanguage << std::endl;
+	}
+	void Work() {
+		std::cout << Name << " is writing " << FavProgrammingLanguage << " code." << std::endl;
+	}
+};
+
+class Teacher:public Employee {
+public:
+	string Subject;
+	void PrepareLesson() {
+		std::cout << Name << " is preparing " << Subject << " lesson." << std::endl;
+	}
+	Teacher(string name, string company, int age, string subject)
+		:Employee(name, company, age)
+	{
+		Subject = subject;
+	}
+
+	void Work() {
+		std::cout << Name << " is teaching " << Subject << " lesson." << std::endl;
+	}
 };
 
 int main()
 {
-	Employee employee1 = Employee("Saldina", "YT-CodeBeauty", 25);
-	employee1.IntroduceYourself();
+	Developer d = Developer("Saldina", "YT-CodeBeauty", 25, "C++");
+	d.FixBug();
+	d.FixBug();
+	d.FixBug();
+	d.AskForPromotion();
 
-	Employee employee2 = Employee("John", "Amazon", 35);
-	employee2.IntroduceYourself();
+	Teacher t = Teacher("Jack", "Cool School", 35, "History");
+	t.PrepareLesson();
+	t.AskForPromotion();
 
-	employee1.AskForPromotion();
-	employee2.AskForPromotion();
+	Employee* e1 = &d;
+	Employee* e2 = &t;
+
+	e1->Work();
+	e2->Work();
 }
