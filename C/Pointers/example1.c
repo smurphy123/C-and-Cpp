c218464d4a37bc5838b90b584ddb641483c66b55
smurphy123 <smurphysmail@gmail.com>
2021-05-30 12:42:53 -0400

    Repo Update

--- C/Pointers/example1.c
+++ C/Pointers/example1.c
@@ -4,9 +4,14 @@ int main()
 {
     int a;
     int *p;
+    a = 10;
     p = &a; // &a = address of a
 
-    printf("%d\n", p);
-    printf("%d\n", *p); // *p - value at address pointed to by p
-    printf("%d\n", &a);
+    printf("Adress of p is %d\n", p);
+    printf("Value at p is %d\n", *p);
+
+    int b = 20;
+    *p = b; // Will the address in p change to point b?
+    printf("Address of P is %d\n", p);
+    printf("Value at p is %d\n", *p);
 }
